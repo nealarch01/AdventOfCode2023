7422e2b7cda59eefe1becfd51f702a21fd395018
Neal Archival <73256760+nealarch01@users.noreply.github.com>
2023-12-01 21:05:58 -0800

    Initial commit

---
+++ Day1/Day1.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func findCalibrationValue(_ value: String) -> String {
+    // Put all the numbers into an array
+    let allNumbers = value.filter { $0.isNumber }
+    let firstNumber = String(allNumbers.first ?? Character(""))
+    let secondNumber = String(allNumbers.last ?? Character(""))
+    return String(firstNumber + secondNumber)
+}
+
+guard let fileContents = try? String(contentsOfFile: "Input.txt") else {
+    print("An error occured reading input file...")
+    exit(1)
+}
+
+let inputs = fileContents.components(separatedBy: "\n")
+
+var sum: Int = 0
+inputs.forEach { input in
+    let calibrationValue = Int(findCalibrationValue(input))!
+    sum += calibrationValue
+}
+
+print("The sum is: \(sum)")
